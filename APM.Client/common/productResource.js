616ed59f82ed827f984c6c9e5c3dd794da72c2b7
AKM <ashish.akm@gmail.com>
2018-12-31 19:03:07 +0530

    V1- List all product except cors

---
+++ APM.Client/common/productResource.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("common.services")
+        .factory("productResource", ["$resource", "appSettings", productResource])
+    function productResource($resource, appSettings) {
+        return $resource("http://localhost:60133/api/products/:id");
+    }
+}());
--- APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM.WebAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
@@ -14,12 +15,14 @@ namespace APM.WebAPI
         {
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+           // var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
